ae11b4aa68e2a3fc1dbc3a985c54ef0c1422912e
Administrator <314892681>
2019-03-20 15:26:46 +0800

    输出打印

---
+++ src/main/java/cn/newer/hr/HrApplication.java
@@ -0,0 +1,13 @@
+package cn.newer.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrApplication.class, args);
+        System.out.println("=======Hr Success To Starting=======");
+    }
+}
